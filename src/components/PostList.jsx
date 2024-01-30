a7e12f16d3fcdfdf3b524bb7bb9ddee3678378e9
Sonam Sherpa <sherpa.sjs@gmail.com>
2024-01-29 23:21:45 -0800

    create form and newpost component with css style

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,44 @@
-import Post from './Post';
-import classes from './PostList.module.css';
+import { useState } from "react";
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostList.module.css";
 
 const PostList = () => {
-  return (
-    <ul className={classes.posts}>
- <Post author = "Sonam" body = "This is Sonam's Post"/>
- <Post author = "Max" body = "This is Max Post"/>
-    </ul>
-   
-  )
-}
+// state for modal to display and hide
+  const [modelVisible, setModelVisible] = useState(true);
+//state for input text
+  const [inputText, setInputText] = useState("");
+//state for author name
+  const [author, setAuthor] = useState("");
 
-export default PostList
+  function hideModalHandler() {
+    setModelVisible(false);
+  }
+
+  function changeBodyHandler(event) {
+    setInputText(event.target.value);
+  }
+  function changeAuthorHandler(event) {
+    setAuthor(event.target.value);
+  }
+  return (
+    <>
+      {modelVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={changeBodyHandler}
+            onAuthorchange={changeAuthorHandler}
+          />
+        </Modal>
+      )}
+
+      <ul className={classes.posts}>
+        <Post author={author} body={inputText} />
+        <Post author="Max" body="This is Max Post" />
+      </ul>
+    </>
+  );
+};
+
+export default PostList;
